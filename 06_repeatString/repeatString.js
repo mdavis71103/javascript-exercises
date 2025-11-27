6cbf5fda8180b7397f2a1bc4b804b836b6a091bb
mdavis71103 <mdavis71103@gmail.com>
2025-11-27 06:09:12 -0500

    Complete Foundatioms Js06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+    if(num <  0) return "ERROR"
+
+    let copiedstr =""
+
+    for(let i = 0; i < num; i++){
+        copiedstr += str;
+    }
+
+    return copiedstr
 };
 
 // Do not edit below this line
